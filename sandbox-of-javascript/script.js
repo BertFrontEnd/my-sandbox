dc76f63d73f932098d118bccfce6021e9132bc10
BertFrontEnd <Berton@yandex.ru>
2020-02-16 22:07:22 +0300

    chore: add some Kata

---
+++ sandbox-of-javascript/script.js
@@ -0,0 +1,67 @@
+//1:
+console.log('1:');
+function solution(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}
+
+console.log(solution('JavaScript'));
+console.log(solution('BertFrontEnd'));
+console.log(solution('RSSchool'));
+console.log(solution('Замок'));
+
+//2:
+console.log('2:');
+function removeChar(str) {
+  return str.substr(1, str.length - 2);
+}
+
+console.log(removeChar('JavaScript'));
+console.log(removeChar('BertFrontEnd'));
+console.log(removeChar('RSSchool'));
+console.log(removeChar('Замок'));
+
+//3:
+console.log('3:');
+function isPalindrome(line) {
+  let string = line.toString().toLowerCase();
+  let strLength = string.length;
+  for (i = 0; i < strLength; i++) {
+    if (string[i] === string[strLength - 1 - i]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+console.log(isPalindrome('anna'));
+console.log(isPalindrome('walter'));
+console.log(isPalindrome(12321));
+console.log(isPalindrome(123456));
+
+//4:
+console.log('4:');
+var isAnagram = function(test, original) {
+  return (
+    test
+      .toLowerCase()
+      .split('')
+      .sort()
+      .join('') ===
+    original
+      .toLowerCase()
+      .split('')
+      .sort()
+      .join('')
+  );
+};
+
+console.log(isAnagram('foefet', 'toffee'));
+console.log(isAnagram('Buckethead', 'DeathCubeK'));
+console.log(isAnagram('Twoo', 'WooT'));
+console.log(isAnagram('dumble', 'bumble'));
+console.log(isAnagram('ound', 'round'));
+console.log(isAnagram('apple', 'pale'));

9a974c78ba7f3342c1df7882746faebf4f056439
BertFrontEnd <Berton@yandex.ru>
2020-08-01 12:32:31 +0300

    feat: add add-remove-active-class

---
+++ javascript-sandbox/021-add-remove-active-class/script.js
@@ -0,0 +1,8 @@
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('a');
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = 'active';
+  }
+}

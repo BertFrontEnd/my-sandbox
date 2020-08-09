6c14e89d34dd1f2553b0375bbd6535a23a989945
BertFrontEnd <Berton@yandex.ru>
2020-08-09 14:35:28 +0300

    chore: edit object.js

--- mixed-sandbox/senior-frontend-developer/06-object.js
+++ mixed-sandbox/senior-frontend-developer/06-object.js
@@ -5,7 +5,14 @@
 // Второй - определенные поля для создаваемого объекта
 
 const person = Object.create(
-  {},
+  {
+    calculateAge() {
+      console.log(
+        'Age from calculateAge:',
+        new Date().getFullYear() - this.birthYear,
+      );
+    },
+  },
   {
     // Создаваемое поле является объектом
     // В отличии от обычного объекта
@@ -29,6 +36,27 @@ const person = Object.create(
       writable: false,
       configurable: false,
     },
+
+    age: {
+      // getters
+      // Возвращает значение
+      // Вызывается поле поле/ключ
+      // Но срабатывает метода get
+      get() {
+        return new Date().getFullYear() - this.birthYear;
+      },
+      // setters
+      // Устанавливает значение
+      // В качестве аргумента передается како-то значение
+      // Которое нужно установить
+      set(value) {
+        // Выводит новое установленное значение
+        // Без изменения объекта
+        console.log('Set age:', value);
+        // Задание цвета фона документа
+        document.body.style.backgroundColor = 'blue';
+      },
+    },
   },
 );
 
@@ -45,9 +73,34 @@ for (let key in person) {
   // Ничего не выводит, если не задан PD {enumerable: true,}
   // Значение по умолчанию {enumerable: false,}
 
-  // Вывод поля объекта и его значение
-  console.log('Key of person:', key, person[key]);
+  // Что бы не пробегаться циклом по прототипу объекта person
+  // В том числе и по первому параметру - первому объекту
+  // Создается условие
+  // Рекомендуется использовать данную проверку
+  // При использовании цикла (for...in) на объекте
+
+  if (person.hasOwnProperty(key)) {
+    // Вывод поля объекта и его значение
+    console.log('Key of person:', key, person[key]);
+  }
 }
+
+// Вызов метода calculateAge() из первого объекта - первого параметра,
+// Объекта person
+person.calculateAge();
+
+// Использование getters
+console.log('getter age:', person.age);
+
+// Использование setters
+// Установка значения ключа/поля age
+// Позволяет выполнить любой код в setters
+let val = (person.age = 35);
+console.log('setter age:', val);
+console.log('age:', person.age);
+
+// Объект person
+console.log('person:', person);
 
 // Для сравнения, обычный объект и цикл for-in
 const newPerson = {

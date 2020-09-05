a7e86f938029bba9f83ee6d95499e7cc30b45e79
BertFrontEnd <Berton@yandex.ru>
2020-09-05 20:52:13 +0300

    feat: add working-analog-clock

---
+++ javascript-sandbox/039-working-analog-clock/script.js
@@ -0,0 +1,16 @@
+const deg = 6;
+const hr = document.getElementById('hr');
+const mn = document.getElementById('mn');
+const sc = document.getElementById('sc');
+
+setInterval(() => {
+  let day = new Date();
+  console.log(day.getHours());
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg;
+
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+}, 1000);

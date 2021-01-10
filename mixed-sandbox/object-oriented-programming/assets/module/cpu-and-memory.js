dd84530ead55593455af89770df2792f5804d3d2
BertFrontEnd <Berton@yandex.ru>
2021-01-10 16:40:55 +0300

    feat: add flying-jet-animation-effect

---
+++ mixed-sandbox/object-oriented-programming/assets/module/cpu-and-memory.js
@@ -0,0 +1,24 @@
+const cpu = {
+  producer: 'AMD',
+  amount: 250,
+  percent: 5,
+  family: 'Ryzen 7',
+  name: 'Ryzen 7 3700X',
+  image: './assets/img/ryzen.png',
+  getAmount() {
+    const discount = this.amount - this.amount * (this.percent / 100);
+    return discount;
+  },
+};
+
+const memory = {
+  producer: 'Hynix',
+  amount: 60,
+  memory: 2048,
+  name: 'Hynix DDR4-2666',
+  family: 'DDR4',
+  image: './assets/img/hynix.png',
+  __proto__: cpu,
+};
+
+export { cpu, memory };
--- mixed-sandbox/object-oriented-programming/script.js
+++ mixed-sandbox/object-oriented-programming/script.js
@@ -1,25 +1,10 @@
-const cpu = {
-  producer: 'AMD',
-  amount: 250,
-  percent: 5,
-  family: 'Ryzen 7',
-  name: 'Ryzen 7 3700X',
-  image: './assets/img/ryzen.png',
-  getAmount() {
-    const discount = this.amount - this.amount * (this.percent / 100);
-    return discount;
-  },
-};
+// Cpu and Memory
+import { cpu, memory } from './assets/module/cpu-and-memory.js';
 
-const memory = {
-  producer: 'Hynix',
-  amount: 60,
-  memory: 2048,
-  name: 'Hynix DDR4-2666',
-  family: 'DDR4',
-  image: './assets/img/hynix.png',
-  __proto__: cpu,
-};
+console.log(cpu);
+console.log(memory);
 
 console.log(cpu.getAmount());
-console.log(memory);
+console.log(memory.getAmount());
+
+// Button and Modern Button

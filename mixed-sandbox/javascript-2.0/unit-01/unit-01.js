3dc5ffbeaee6da0c7762e02be9740f145e11519d
BertFrontEnd <Berton@yandex.ru>
2021-01-12 01:01:48 +0300

    chore: change structure unit-01 of javascript-2.0

--- mixed-sandbox/javascript-2.0/unit-01/unit-01.js
+++ mixed-sandbox/javascript-2.0/unit-01/unit-01.js
@@ -1,35 +1,74 @@
+// Task 01
 console.log('Berton');
+
+// Task 02
 console.log(08);
+
+// Task 03
 console.log('Welcome ' + 'to ' + 'course');
+
+// Task 04
 /* alert(2020); */
+
+// Task 05
 /* alert(2020 - 200); */
+
+// Task 06
 document.getElementById('one').innerHTML = 'Welcome';
+
+// Task 07
 document.getElementById('two').innerHTML = 12 * 12;
+
+// Task 08
 document.querySelector('.one').innerHTML = 'Hello World';
-document.querySelector('span').innerHTML = 'World';
+
+// Task 09
+document.querySelector('.span').innerHTML = 'World';
+
+// Task 10
 document.querySelector('.three').innerHTML = `<h3>Random Text H3</h3>`;
+
+// Task 11
 document.querySelector('.four').innerHTML = `<h4>Random Text H4</h4>`;
 document.querySelector('.four').innerHTML += `<p>Random Text Paragraph</p>`;
+
+// Task 12
 const numberPi = (document.querySelector('.five').innerHTML = 3.14159);
+
+// Task 13
 const div7 = (document.querySelector('.seven').innerHTML =
-  '<img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="">');
+  '<img src="./assets/img/startup.svg" width="50px "alt="">');
+
+// Task 14
 const z1 = 6;
 const z2 = 3;
 document.querySelector('.multiplication').innerHTML = z1 * z2;
+
+// Task 15
 const y1 = 6;
 const y2 = 3;
 document.querySelector('.division').innerHTML = y1 / y2;
+
+// Task 16
 const x1 = 'Hello';
 const x2 = 5;
-document.querySelector('.division').innerHTML = x1 + x2;
+document.querySelector('.sum').innerHTML = x1 + x2;
+
+// Task 17
 const d1 = document.querySelector('.test-1');
 console.log(d1);
+
+// Task 18
 let d2 = document.querySelector('.test-2');
 console.log(d2);
 d2 = 5;
 console.log(d2);
+
+// Task 19
 let divS3 = document.querySelector('.s3');
 console.log(divS3);
 divS3 = document.querySelector('.s4');
 console.log(divS3);
+
+// Task 20
 /* document.querySelector('body').innerHTML = ''; */

b80073dffc621509286df113aad2ee5b60462106
BertFrontEnd <Berton@yandex.ru>
2021-01-11 18:22:34 +0300

    feat: add unit-01 of javascript 2.0

---
+++ mixed-sandbox/javascript-2.0/unit-01/unit-01.js
@@ -0,0 +1,35 @@
+console.log('Berton');
+console.log(08);
+console.log('Welcome ' + 'to ' + 'course');
+/* alert(2020); */
+/* alert(2020 - 200); */
+document.getElementById('one').innerHTML = 'Welcome';
+document.getElementById('two').innerHTML = 12 * 12;
+document.querySelector('.one').innerHTML = 'Hello World';
+document.querySelector('span').innerHTML = 'World';
+document.querySelector('.three').innerHTML = `<h3>Random Text H3</h3>`;
+document.querySelector('.four').innerHTML = `<h4>Random Text H4</h4>`;
+document.querySelector('.four').innerHTML += `<p>Random Text Paragraph</p>`;
+const numberPi = (document.querySelector('.five').innerHTML = 3.14159);
+const div7 = (document.querySelector('.seven').innerHTML =
+  '<img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="">');
+const z1 = 6;
+const z2 = 3;
+document.querySelector('.multiplication').innerHTML = z1 * z2;
+const y1 = 6;
+const y2 = 3;
+document.querySelector('.division').innerHTML = y1 / y2;
+const x1 = 'Hello';
+const x2 = 5;
+document.querySelector('.division').innerHTML = x1 + x2;
+const d1 = document.querySelector('.test-1');
+console.log(d1);
+let d2 = document.querySelector('.test-2');
+console.log(d2);
+d2 = 5;
+console.log(d2);
+let divS3 = document.querySelector('.s3');
+console.log(divS3);
+divS3 = document.querySelector('.s4');
+console.log(divS3);
+/* document.querySelector('body').innerHTML = ''; */

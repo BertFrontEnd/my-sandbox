127722af3f3a9d575d35874192d30e25ccccf672
BertFrontEnd <Berton@yandex.ru>
2021-01-16 18:57:12 +0300

    feat: add 3d-mousemove-parallax

---
+++ javascript-sandbox/059-3d-mousemove-paarallax/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('mousemove', function (e) {
+  this.querySelectorAll('.layer').forEach((layer) => {
+    let speed = layer.getAttribute('data-speed');
+    let x = (window.innerWidth - e.pageX * speed) / 100;
+    let y = (window.innerWidth - e.pageY * speed) / 100;
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+});

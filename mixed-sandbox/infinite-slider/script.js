8cc892a26c3eda232701336fba4f5b64cc239fd8
BertFrontEnd <Berton@yandex.ru>
2021-09-03 22:20:37 +0300

    feat: add infinite-slider

---
+++ mixed-sandbox/infinite-slider/script.js
@@ -0,0 +1,59 @@
+const next = document.querySelector('#btn-next');
+const prev = document.querySelector('#btn-prev');
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+
+let index = 0;
+
+const activeSlide = (n) => {
+  console.log(n);
+  for (let slide of slides) {
+    slide.classList.remove('active');
+  }
+  slides[n].classList.add('active');
+};
+
+const activeDot = (n) => {
+  console.log(n);
+  for (let dot of dots) {
+    dot.classList.remove('active');
+  }
+  dots[n].classList.add('active');
+};
+
+const prepareCurrentSlide = (i) => {
+  activeSlide(i);
+  activeDot(i);
+};
+
+const nextSlide = () => {
+  if (index == slides.length - 1) {
+    index = 0;
+    prepareCurrentSlide(index);
+  } else {
+    index++;
+    prepareCurrentSlide(index);
+  }
+};
+
+const prevSlide = () => {
+  if (index == 0) {
+    index = slides.length - 1;
+    prepareCurrentSlide(index);
+  } else {
+    index--;
+    prepareCurrentSlide(index);
+  }
+};
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+
+dots.forEach((item, indexDot) => {
+  item.addEventListener('click', () => {
+    index = indexDot;
+    prepareCurrentSlide(index);
+  });
+});
+
+setInterval(nextSlide, 2500);

fdb737ea3accb2735d62ad01d69581b9d832bd47
BertFrontEnd <Berton@yandex.ru>
2021-09-05 23:37:04 +0300

    feat: add base structure

---
+++ mixed-sandbox/carusel-swiper/script.js
@@ -0,0 +1,7 @@
+const items = document.querySelectorAll('.item');
+let currentItem = 0;
+let isEnabled = true;
+
+function changeCurrentItem(n) {
+  currentItem = (n + items.length) % items.length;
+}

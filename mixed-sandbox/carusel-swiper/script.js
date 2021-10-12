742928ac68237b6e6300850651df98aee5b504b7
BertFrontEnd <Berton@yandex.ru>
2021-10-12 22:04:12 +0300

    chore: edit carousel

--- mixed-sandbox/carusel-swiper/script.js
+++ mixed-sandbox/carusel-swiper/script.js
@@ -1,7 +1,53 @@
-const items = document.querySelectorAll('.item');
+let items = document.querySelectorAll('.carousel .item');
 let currentItem = 0;
 let isEnabled = true;
 
 function changeCurrentItem(n) {
   currentItem = (n + items.length) % items.length;
 }
+
+function hideItem(direction) {
+  isEnabled = false;
+  items[currentItem].classList.add(direction);
+  items[currentItem].addEventListener('animationend', function () {
+    this.classList.remove('active', direction);
+  });
+}
+
+function showItem(direction) {
+  items[currentItem].classList.add('next', direction);
+  items[currentItem].addEventListener('animationend', function () {
+    this.classList.remove('next', direction);
+    this.classList.add('active');
+    isEnabled = true;
+  });
+}
+
+function nextItem(n) {
+  hideItem('to-left');
+  changeCurrentItem(n + 1);
+  showItem('from-right');
+}
+
+function previousItem(n) {
+  hideItem('to-right');
+  changeCurrentItem(n - 1);
+  showItem('from-left');
+}
+
+document.querySelector('.control.left').addEventListener('click', function () {
+  if (isEnabled) {
+    previousItem(currentItem);
+  }
+});
+
+document.querySelector('.control.right').addEventListener('click', function () {
+  if (isEnabled) {
+    nextItem(currentItem);
+  }
+});
+
+const swipeDetect = () => {};
+
+let element = document.querySelector('.carousel');
+swipeDetect(element);

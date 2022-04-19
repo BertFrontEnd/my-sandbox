bfb7c9b5fa2f0fecce6407150db1e87f34273d60
BertFrontEnd <Berton@yandex.ru>
2022-04-19 21:07:04 +0300

    chore: edit unit-18 of javascript-2.0

--- mixed-sandbox/javascript-2.0/unit-18/unit-18.js
+++ mixed-sandbox/javascript-2.0/unit-18/unit-18.js
@@ -124,6 +124,26 @@ document.querySelector('.b-9').onclick = t9;
 // Task 10 ============================================
 /*  Дан массив a10 = {name: ivan, age: 15, sex: 1, id: 45} - преобразуйте его в query строку ( так передаются GET параметры). Найдите описание что такое query строка самостоятельно. Разделитель - амперсанд. Результат присвойте a10_res. Запускаться решение должно при вызове функции t10. Допускается лишний амперсанд в конце строки!!! */
 
-function t10() {}
+function t10() {
+  let a10 = {
+    name: 'ivan',
+    age: 15,
+    sex: 1,
+    id: 45,
+  };
+
+  /* a10.forEach((elem) => console.log(elem)); */
+  for (let key in a10) {
+    a10_res += `${key} = ${a10[key]}&`;
+  }
+
+  let arr = [...a10_res]
+    .slice(0, length - 1)
+    .filter((elem) => elem != ' ')
+    .join('');
+
+  console.log(arr);
+  document.querySelector('.out-10').innerHTML = arr;
+}
 
 document.querySelector('.b-10').onclick = t10;

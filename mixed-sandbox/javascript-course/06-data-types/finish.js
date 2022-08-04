cdc78b199819e66bfe16731a2cf8bed6502fe70c
BertFrontEnd <Berton@yandex.ru>
2022-08-04 22:15:10 +0300

    feat: add solution 06-data-types of javascript-course

--- mixed-sandbox/javascript-course/06-data-types/finish.js
+++ mixed-sandbox/javascript-course/06-data-types/finish.js
@@ -12,3 +12,19 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+const str = 'Berton';
+const num = 42;
+const booleanVariable = true;
+const nullVariable = null;
+const undefinedVariable = undefined;
+const obj = { str: str, num: num, booleanVariable: booleanVariable };
+const arr = [str, num, booleanVariable];
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof booleanVariable);
+console.log(typeof nullVariable);
+console.log(typeof undefinedVariable);
+console.log(typeof obj);
+console.log(typeof arr);

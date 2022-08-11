a58929af015645e5231619492689be0d8077e710
BertFrontEnd <Berton@yandex.ru>
2022-08-12 00:17:26 +0300

    feat: add solution 13-string-length of javascript-course

--- mixed-sandbox/javascript-course/13-string-length/finish.js
+++ mixed-sandbox/javascript-course/13-string-length/finish.js
@@ -2,5 +2,9 @@
  *
  * 1. Создайте переменную и присвойте ей любую строку
  *
- * 2. Виведите в консоль длину этой строки
+ * 2. Выведите в консоль длину этой строки
  */
+
+const myName = 'My name is Sergey Bry';
+const nameLength = myName.length;
+console.log(nameLength);

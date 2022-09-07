c6ba33884d4423a4a627ad8ace1c7499edce7382
BertFrontEnd <Berton@yandex.ru>
2022-09-07 19:37:23 +0300

    feat: add solution 15-replace-part-string of javascript-course

--- mixed-sandbox/javascript-course/15-replace-part-string/finish.js
+++ mixed-sandbox/javascript-course/15-replace-part-string/finish.js
@@ -6,3 +6,13 @@
  *
  * 3. Выведите значение второй переменной в консоль
  */
+
+const message01 = 'Good Morning';
+console.log(message01);
+const newMessage01 = message01.split(' ').slice(0, 1).join('') + ' Evening';
+console.log(newMessage01);
+
+const message02 = 'Good Morning';
+console.log(message02);
+const newMessage02 = message02.replace('Morning', 'Evening');
+console.log(newMessage02);

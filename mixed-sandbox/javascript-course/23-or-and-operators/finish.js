06666787b92917039f33b199a3c0fb723ed1010a
BertFrontEnd <Berton@yandex.ru>
2022-10-11 22:38:01 +0300

    feat: add solution 23-or-and-operators

--- mixed-sandbox/javascript-course/23-or-and-operators/finish.js
+++ mixed-sandbox/javascript-course/23-or-and-operators/finish.js
@@ -3,4 +3,8 @@
  * Что будет выведено в консоль?
  */
 
-console.log(3 || (true && null) || false)
+console.log(3 || (true && null) || false); // 3
+
+// true && null -> null
+// 3 || null -> 3
+// 3 || false -> 3

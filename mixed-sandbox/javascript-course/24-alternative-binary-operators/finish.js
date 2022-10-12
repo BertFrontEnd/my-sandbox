37748162b2d2c8cc2f2c24aa16eb1b8aa17b6d0e
BertFrontEnd <Berton@yandex.ru>
2022-10-12 22:20:41 +0300

    feat: add solution 24-alternative-binary-operators

--- mixed-sandbox/javascript-course/24-alternative-binary-operators/finish.js
+++ mixed-sandbox/javascript-course/24-alternative-binary-operators/finish.js
@@ -3,16 +3,16 @@
  * В каждом выражении замените два оператора на один бинарный
  */
 
-let a = 10
+let a = 10;
 
-a = a + 1
-console.log(a) // 11
+a += 1;
+console.log(a); // 11
 
-a = a * 2
-console.log(a) // 22
+a *= 2;
+console.log(a); // 22
 
-a = a - 5
-console.log(a) // 17
+a -= 5;
+console.log(a); // 17
 
-a = a / 2
-console.log(a) // 8.5
+a /= 2;
+console.log(a); // 8.5

736124c1e4efcb9f1954669a1b541de455c5dd8c
BertFrontEnd <Berton@yandex.ru>
2022-10-13 23:33:48 +0300

    feat: add solution 28-let

--- mixed-sandbox/javascript-course/28-let/finish.js
+++ mixed-sandbox/javascript-course/28-let/finish.js
@@ -3,12 +3,12 @@
  * Измените код так, чтобы ошибка исчезла
  */
 
-const myFavoriteAnimal = 'Monkey'
+let myFavoriteAnimal = 'Monkey';
 
-console.log(myFavoriteAnimal)
+console.log(myFavoriteAnimal);
 // 'Monkey'
 
-const myFavoriteAnimal = 'Cat'
+myFavoriteAnimal = 'Cat';
 
-console.log(myFavoriteAnimal)
+console.log(myFavoriteAnimal);
 // 'Cat'

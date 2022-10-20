128f98639ced4c9c4c57d537780cdfe1b40c207a
BertFrontEnd <Berton@yandex.ru>
2022-10-20 23:01:18 +0300

    feat: add solution 41-find-single-post

--- mixed-sandbox/javascript-course/41-find-single-post/finish.js
+++ mixed-sandbox/javascript-course/41-find-single-post/finish.js
@@ -16,8 +16,12 @@ const posts = [
   { postId: 1355, commentsQuantity: 5 },
   { postId: 5131, commentsQuantity: 13 },
   { postId: 6134, commentsQuantity: 2 },
-]
+];
 
-console.log(findPostById(6134, posts)) // { postId: 6134, commentsQuantity: 2 }
+const findPostById = (id, postsArr) => {
+  return postsArr.find((el) => el.postId == id);
+};
 
-console.log(findPostById(4511, posts)) // undefined
+console.log(findPostById(6134, posts)); // { postId: 6134, commentsQuantity: 2 }
+
+console.log(findPostById(4511, posts)); // undefined

9462cd099dc36da025be953cc07102caeb9c7b53
BertFrontEnd <Berton@yandex.ru>
2022-10-26 22:51:27 +0300

    chore: edit coolours

--- mixed-sandbox/coolours/script.js
+++ mixed-sandbox/coolours/script.js
@@ -2,6 +2,12 @@ console.log('Working...');
 
 const cols = document.querySelectorAll('.col');
 
+document.addEventListener('keydown', (e) => {
+  if (e.code.toLocaleLowerCase() === 'space') {
+    setRandomColors();
+  }
+});
+
 // Generate Random Color
 const generateRandomColor = () => {
   const hexCodes = '0123456789ABCDEF';
@@ -15,9 +21,26 @@ const generateRandomColor = () => {
   return `#${color}`;
 };
 
+// Set Color with Chroma.js
+const setTextColor = (text, color) => {
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > 0.5 ? 'black' : 'white';
+};
+
 // Set Random Color
 const setRandomColors = () => {
-  cols.forEach((col) => (col.style.backgroundColor = generateRandomColor()));
+  cols.forEach((col) => {
+    const text = col.querySelector('h2');
+    const button = col.querySelector('button');
+    const color = chroma.random();
+    /* const color = generateRandomColor(); */
+
+    text.textContent = color;
+    col.style.backgroundColor = color;
+
+    setTextColor(text, color);
+    setTextColor(button, color);
+  });
 };
 
 setRandomColors();

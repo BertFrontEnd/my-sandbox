68d3ee26b43cf3208808681250cd2ceb088ddf2c
BertFrontEnd <Berton@yandex.ru>
2022-10-27 22:39:00 +0300

    chore: edit coolours

--- mixed-sandbox/coolours/script.js
+++ mixed-sandbox/coolours/script.js
@@ -3,8 +3,49 @@ console.log('Working...');
 const cols = document.querySelectorAll('.col');
 
 document.addEventListener('keydown', (e) => {
+  e.preventDefault();
   if (e.code.toLocaleLowerCase() === 'space') {
     setRandomColors();
+  }
+});
+
+// Copy Color
+const copyToClickBoard = (text) => {
+  return navigator.clipboard.writeText(text);
+};
+
+// Set Color to Hash
+const updateColorsHash = (colors = []) => {
+  document.location.hash = colors
+    .map((col) => col.toString().substring(1))
+    .join('-');
+};
+
+// Get Color from Hash
+const getColorFromHash = () => {
+  if (document.location.hash.length > 1) {
+    return document.location.hash
+      .substring(1)
+      .split('-')
+      .map((color) => `#${color}`);
+  }
+
+  return [];
+};
+
+document.addEventListener('click', (e) => {
+  const type = e.target.dataset.type;
+
+  if (type === 'lock') {
+    const node =
+      e.target.tagName.toLocaleLowerCase() === 'i'
+        ? e.target
+        : e.target.children[0];
+
+    node.classList.toggle('fa-lock-open');
+    node.classList.toggle('fa-lock');
+  } else if (type === 'copy') {
+    copyToClickBoard(e.target.textContent);
   }
 });
 
@@ -28,12 +69,30 @@ const setTextColor = (text, color) => {
 };
 
 // Set Random Color
-const setRandomColors = () => {
-  cols.forEach((col) => {
+const setRandomColors = (isInitial) => {
+  const colors = isInitial ? getColorFromHash() : [];
+
+  cols.forEach((col, idx) => {
+    const isLocked = col.querySelector('i').classList.contains('fa-lock');
     const text = col.querySelector('h2');
     const button = col.querySelector('button');
-    const color = chroma.random();
     /* const color = generateRandomColor(); */
+
+    if (isLocked) {
+      colors.push(text.textContent);
+      console.log(colors);
+      return;
+    }
+
+    const color = isInitial
+      ? colors[idx]
+        ? colors[idx]
+        : chroma.random()
+      : chroma.random();
+
+    if (!isInitial) {
+      colors.push(color);
+    }
 
     text.textContent = color;
     col.style.backgroundColor = color;
@@ -41,6 +100,8 @@ const setRandomColors = () => {
     setTextColor(text, color);
     setTextColor(button, color);
   });
+
+  updateColorsHash(colors);
 };
 
-setRandomColors();
+setRandomColors(true);

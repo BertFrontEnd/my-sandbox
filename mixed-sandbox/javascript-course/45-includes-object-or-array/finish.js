9b00087eb0f7e49db91e6cc1613ed521fb682a17
BertFrontEnd <Berton@yandex.ru>
2022-10-29 19:47:47 +0300

    chore: edit solution 45-includes-object-or-array

--- mixed-sandbox/javascript-course/45-includes-object-or-array/finish.js
+++ mixed-sandbox/javascript-course/45-includes-object-or-array/finish.js
@@ -14,31 +14,25 @@ const tags = [
   ['css', 'flexbox'],
   ['html', 'web-browser'],
 ];
-
 const fruits = [
   { title: 'Orange', quantity: 10 },
   { title: 'Banana', quantity: 5 },
   { title: 'Apple', quantity: 25 },
 ];
-
 const primitiveTypesArray = [25, 'x', true, undefined, null];
 
 const isElementInArray = (searchElement, inputArray) => {
-  if (typeof searchElement == 'object') {
-    return inputArray.some(
-      (el) => JSON.stringify(el) === JSON.stringify(searchElement),
-    );
+  if (typeof searchElement !== 'object') {
+    return inputArray.includes(searchElement);
   }
 
-  return inputArray.includes(searchElement);
+  return inputArray.some(
+    (el) => JSON.stringify(el) === JSON.stringify(searchElement),
+  );
 };
 
 console.log(isElementInArray(['css', 'flexbox'], tags)); // true
-
 console.log(isElementInArray(['flexbox', 'css'], tags)); // false
-
 console.log(isElementInArray({ title: 'Apple', quantity: 25 }, fruits)); // true
-
 console.log(isElementInArray({ title: 'Banana' }, fruits)); // false
-
 console.log(isElementInArray(25, primitiveTypesArray)); // true

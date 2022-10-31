d55f12119beac80afea329ad91787ed8411f00a3
BertFrontEnd <Berton@yandex.ru>
2022-10-31 23:22:00 +0300

    feat: add solution 46-push-to-array-if-not-exists

--- mixed-sandbox/javascript-course/46-push-to-array-if-not-exists/finish.js
+++ mixed-sandbox/javascript-course/46-push-to-array-if-not-exists/finish.js
@@ -10,16 +10,23 @@
  * ПРИМЕЧАНИЕ: Мы предполагаем, что "inputArray" содержит элементы только примитивных типов
  */
 
-const myNumbers = [123, 50, 27]
+const myNumbers = [123, 50, 27];
 
-pushIfUnique(myNumbers, 50) // "50 уже в массиве"
-console.log(myNumbers) // [123, 50, 27]
+const pushIfUnique = (inputArray, newElement) => {
+  if (inputArray.includes(newElement)) {
+    return console.log(`${newElement} уже находится в массиве`);
+  }
+  inputArray.push(newElement);
+};
 
-pushIfUnique(myNumbers, 80)
-console.log(myNumbers) // [123, 50, 27, 80]
+pushIfUnique(myNumbers, 50); // "50 уже в массиве"
+console.log(myNumbers); // [123, 50, 27]
 
-pushIfUnique(myNumbers, 80) // "80 уже в массиве"
-console.log(myNumbers) // [123, 50, 27, 80]
+pushIfUnique(myNumbers, 80);
+console.log(myNumbers); // [123, 50, 27, 80]
 
-pushIfUnique(myNumbers, 77)
-console.log(myNumbers) // [123, 50, 27, 80, 77]
+pushIfUnique(myNumbers, 80); // "80 уже в массиве"
+console.log(myNumbers); // [123, 50, 27, 80]
+
+pushIfUnique(myNumbers, 77);
+console.log(myNumbers); // [123, 50, 27, 80, 77]

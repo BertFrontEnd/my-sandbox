5a6bec7fa30bf736c3343950f96dc8eca4e980b9
BertFrontEnd <Berton@yandex.ru>
2022-11-19 13:12:00 +0300

    feat: add solution 54-array-destructuring

--- mixed-sandbox/javascript-course/54-array-destructuring/finish.js
+++ mixed-sandbox/javascript-course/54-array-destructuring/finish.js
@@ -6,15 +6,15 @@
  * Используйте деструктуризацию массива
  */
 
-const processQuantities = (/* parameters */) => {
-  console.log(minQty) // 8
-  console.log(maxQty) // 29
-  console.log(defaultQty) // 10 в первом вызове, 0 во втором вызове
-  return defaultQty + maxQty - minQty
-}
+const processQuantities = ([minQty, maxQty, defaultQty = 0]) => {
+  console.log(minQty); // 8
+  console.log(maxQty); // 29
+  console.log(defaultQty); // 10 в первом вызове, 0 во втором вызове
+  return defaultQty + maxQty - minQty;
+};
 
-const inputQuantities1 = [8, 29, 10]
-console.log(processQuantities(inputQuantities1)) // 31
+const inputQuantities1 = [8, 29, 10];
+console.log(processQuantities(inputQuantities1)); // 31
 
-const inputQuantities2 = [8, 29]
-console.log(processQuantities(inputQuantities2)) // 21
+const inputQuantities2 = [8, 29];
+console.log(processQuantities(inputQuantities2)); // 21

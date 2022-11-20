09d868a1647ac73d5b2da4c5de9bdf86f9b06dfa
BertFrontEnd <Berton@yandex.ru>
2022-11-20 22:56:00 +0300

    chore: edit some funcrion & add showHideIcons

--- mixed-sandbox/draggable-Image-slider/script.js
+++ mixed-sandbox/draggable-Image-slider/script.js
@@ -1,7 +1,26 @@
 const carousel = document.querySelector('.carousel');
+const firstImage = carousel.querySelectorAll('img')[0];
+const arrowIcons = document.querySelectorAll('.wrapper i');
 
 let isDragStart = false;
 let prevPageX, prevScrollLeft;
+let firstImageWidth = firstImage.clientWidth + 14; // получение ширины первого изображения и добавление значения поля в 14 пикселей
+let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // получение максимальной ширины прокрутки - разность всей ширины карусели и видимой части карусели
+
+const showHideIcons = () => {
+  arrowIcons[0].style.display = carousel.scrollLeft == 0 ? 'none' : 'block'; // если карусель находится в крайнем левом положении - на первой картинке - скрыть иконку "влево", иначе отобразить
+  arrowIcons[1].style.display =
+    carousel.scrollLeft == scrollWidth ? 'none' : 'block'; // если карусель находится в крайнем правом положении - на последней картинке - скрыть иконку "влево", иначе отобразить
+};
+
+arrowIcons.forEach((icon) => {
+  icon.addEventListener('click', () => {
+    carousel.scrollLeft +=
+      icon.id == 'left' ? -firstImageWidth : firstImageWidth; // если клик по иконке "влево" - прокрутить карусель влево на ширину первой картинки, иначе - вправо
+    setTimeout(() => showHideIcons(), 60);
+    console.log(carousel.scrollLeft);
+  });
+});
 
 const dragStart = (e) => {
   // обновление значения глобальных переменных при нажатии кнопки мыши
@@ -14,14 +33,14 @@ const dragging = (e) => {
   // прокрутка изображений/карусели влево по указателю мыши
   e.preventDefault();
   if (!isDragStart) return;
+  carousel.classList.add('dragging');
   let positionDiff = e.pageX - prevPageX; // разница координаты X мыши между моментом начала перемещения/клика и текущим положением - количество пикселей, на которое перемешена мышь
   carousel.scrollLeft = prevScrollLeft - positionDiff; // разница между начальным количеством пикселей, на которое уже перемещена карусель, и количеством пикселей, на которое перемещается мышь
-  console.log('prevScrollLeft:', prevScrollLeft);
-  console.log('positionDiff:', positionDiff);
 };
 
 const dragStop = () => {
   isDragStart = false;
+  carousel.classList.remove('dragging');
 };
 
 carousel.addEventListener('mousedown', dragStart);

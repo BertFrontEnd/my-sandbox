1be51007b984f342b1ea368de89415e17e94c47d
BertFrontEnd <Berton@yandex.ru>
2022-12-04 13:51:50 +0300

    feat: add solution 59-extend-array

--- mixed-sandbox/javascript-course/59-extend-array/finish.js
+++ mixed-sandbox/javascript-course/59-extend-array/finish.js
@@ -13,3 +13,29 @@
  * 4. Убедитесь, что остальные методы массивов такие,
  * как "forEach", "map" также доступны
  */
+
+class ExtendedArray extends Array {
+  sum() {
+    return this.reduce((acc, el) => acc + el, 0);
+  }
+  onlyNumbers() {
+    return this.filter((el) => typeof el === 'number');
+  }
+}
+
+const arr1 = [1, 2, 3];
+const newArr1 = new ExtendedArray(...arr1);
+console.log('1:', newArr1.sum());
+
+const arr2 = [1, 2, 3, 'a', 'b', 'c'];
+const newArr2 = new ExtendedArray(...arr2);
+console.log('2:', newArr2.onlyNumbers());
+
+const arr3 = [10, 9, 8];
+const newArr3 = new ExtendedArray(...arr3);
+newArr3.forEach((el) => console.log('3:', el));
+
+/* // Экземпляры Array не имеют доступа к методам ExtendedArray
+const arr4 = [7, 9, 11];
+const newArr4 = new Array(arr4);
+console.log('4:', newArr4.sum()); */

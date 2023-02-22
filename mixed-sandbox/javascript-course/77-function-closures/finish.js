cfabc59cb728b7036cf7914ea1ff63b702816234
BertFrontEnd <Berton@yandex.ru>
2023-02-22 20:34:47 +0300

    feat: add solution 77-function-closures

--- mixed-sandbox/javascript-course/77-function-closures/finish.js
+++ mixed-sandbox/javascript-course/77-function-closures/finish.js
@@ -14,6 +14,23 @@
  *  - changeGreeting
  */
 
+const createGreeting = () => {
+  let greetingString = 'Hey, this is';
+
+  function greet(currentName) {
+    return `${greetingString} ${currentName}`;
+  }
+
+  function changeGreeting(message) {
+    greetingString = message;
+  }
+
+  return {
+    greet,
+    changeGreeting,
+  };
+};
+
 const greeting1 = createGreeting();
 
 console.log(greeting1.greet('Bob'));

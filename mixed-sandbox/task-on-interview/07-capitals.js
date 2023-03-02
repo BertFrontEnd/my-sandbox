91a2d1b3bea4302b2ef7b75262e0cb9e1ecc3bef
BertFrontEnd <Berton@yandex.ru>
2023-03-02 20:49:06 +0300

    feat: add 07-capitals function

--- mixed-sandbox/task-on-interview/07-capitals.js
+++ mixed-sandbox/task-on-interview/07-capitals.js
@@ -1,26 +1,59 @@
 // Mine
-const capitalMine = (string) => console.log('Mine:', capitalMine('abcd'));
-console.log('Mine:', capitalMine('cwAt'));
+const capitalMine = (string) => {
+  const arrIdx = [];
+
+  string
+    .split('')
+    .forEach((el, idx) => (el.toUpperCase() === el ? arrIdx.push(idx) : ''));
+
+  return arrIdx;
+};
+
+console.log('Mine:', capitalMine('CodEWaRs'));
+console.log('Mine:', capitalMine('justForTest'));
 
 // Base
-/* const capitalBase = (string) => {
+const capitalBase = (string) => {
+  const bigLetters = string.toUpperCase().split('');
+  const smallLetters = string.split('');
+  const res = [];
 
+  for (let i = 0; i < string.length; i++) {
+    if (smallLetters[i] === bigLetters[i]) {
+      res.push(i);
+    }
+  }
+
+  return res;
 };
 
-console.log('Base:', capitalBase('abcd'));
-console.log('Base:', capitalBase('cwAt')); */
+console.log('Base:', capitalBase('CodEWaRs'));
+console.log('Base:', capitalBase('justForTest'));
 
 // Advanced
-/* const capitalAdvanced = (string) => {
+const capitalAdvanced = (string) => {
+  const res = [];
 
+  string.split('').forEach((letter, index) => {
+    if (letter === letter.toUpperCase()) {
+      res.push(index);
+    }
+  });
+
+  return res;
 };
 
-console.log('Advanced:', capitalAdvanced('abcd'));
-console.log('Advanced:', capitalAdvanced('cwAt')); */
+console.log('Advanced:', capitalAdvanced('CodEWaRs'));
+console.log('Advanced:', capitalAdvanced('justForTest'));
 
 // ES6
-/* const capitalES6 = (string) =>
+const capitalES6 = (string) =>
+  string.split('').reduce((result, letter, index) => {
+    if (letter === letter.toUpperCase()) {
+      result.push(index);
+    }
+    return result;
+  }, []);
 
-
-console.log('ES6:', capitalES6('abcd'));
-console.log('ES6:', capitalES6('cwAt')); */
+console.log('ES6:', capitalES6('CodEWaRs'));
+console.log('ES6:', capitalES6('justForTest'));

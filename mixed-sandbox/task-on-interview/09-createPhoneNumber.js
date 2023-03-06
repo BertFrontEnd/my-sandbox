26173ff4c3df6c9df9d752976c62cb8421a35a17
BertFrontEnd <Berton@yandex.ru>
2023-03-06 18:40:19 +0300

    chore: edit createPhoneNumber function

--- mixed-sandbox/task-on-interview/09-createPhoneNumber.js
+++ mixed-sandbox/task-on-interview/09-createPhoneNumber.js
@@ -9,19 +9,25 @@ console.log('Mine:', createPhoneNumberMine(123456789));
 console.log('Mine:', createPhoneNumberMine(555095611));
 
 // Base
-const createPhoneNumberBase = (number) => {};
+const createPhoneNumberBase = (number) => {
+  let numArr = number.toString().split('');
+
+  numArr.splice(0, 0, '(');
+  numArr.splice(4, 0, ')');
+  numArr.splice(5, 0, ' ');
+  numArr.splice(9, 0, '-');
+
+  return numArr.join('');
+};
 
 console.log('Base:', createPhoneNumberBase(123456789));
 console.log('Base:', createPhoneNumberBase(555095611));
 
 // Advanced
-const createPhoneNumberAdvanced = (number) => {};
+const createPhoneNumberAdvanced = (number) => {
+  const strNum = number.toString();
+  return `(${strNum.slice(0, 3)}) ${strNum.slice(3, 6)}-${strNum.slice(6, 9)}`;
+};
 
 console.log('Advanced:', createPhoneNumberAdvanced(123456789));
 console.log('Advanced:', createPhoneNumberAdvanced(555095611));
-
-// ES6
-const createPhoneNumberS6 = (number) => {};
-
-console.log('ES6:', createPhoneNumberAdvanced(123456789));
-console.log('ES6:', createPhoneNumberAdvanced(555095611));
